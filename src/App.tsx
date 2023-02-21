21399516660ae79951c201fe42c81252998c45a7
lyran10 <liranramekar629@gmail.com>
2023-02-21 14:01:05 +0200

    Authentication

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {Routes,Route} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="">
+     
+    </div>
+  );
+}
+
+export default App;
